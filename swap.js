ddf9fdf285af666f3530fb8d432d5901c57dbd3d
AlaqraAlik <alikalaqra@gmail.com>
2018-02-28 09:49:41 +0200

    swap

--- swap.js
+++ swap.js
@@ -1,6 +1,25 @@
 	// Given a string, swap the case for each of the letters.
 	// IbrAHiM --> iBRahIm
 	// ToYPRoblEm --> tOyprOBLeM
-	function swap(input){
-		//your code is here
+	function swap(input)
+     {  var arr=input.split("")
+         var x
+		  for (var i =0;i<arr.length; i++) {
+		 
+		   if(arr[i]===arr[i].toLowerCase())
+		   {
+		   
+		   	x=arr[i].toUpperCase()
+
+           }
+
+		   if(arr[i]===arr[i].toUpperCase())
+		   {
+		   	x=arr[i].toLowerCase()
+		   	debugger;
+		   }
+           
+		}
+		return x
+
 	}
