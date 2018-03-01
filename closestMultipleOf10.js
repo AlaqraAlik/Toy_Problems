7fd2c181245fd53c45a34ffc728d2ddbaec41360
AlaqraAlik <alikalaqra@gmail.com>
2018-03-01 09:47:38 +0200

    cc

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -4,5 +4,24 @@
 	// 25 ===> 30
 	// 37 ===> 40
  function closestMultipleOf10(num) {
- 	//your code is here
+      
+
+    
+
+    var m=num.toString()
+    var k=m.split("")
+    var c=0
+    for(var i=0;i<k.length;i++)
+    {
+    	 c=k[i]
+    }
+       if(k[i]<5)
+       {
+       	parseInt(k[i]="0")
+       }
+
+       return parseInt(k)
+
+
+ 	
  }
